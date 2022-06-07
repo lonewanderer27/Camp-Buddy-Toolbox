92d6aa6eb95b3c67484c33e9471d1e5acc3ad55e
lonewanderer27 <adrianejames27@gmail.com>
2022-06-07 14:30:02 +0800

    added a GTK-inspired About window and moved tool functions into separate tools py file

---
+++ src/about.py
@@ -0,0 +1,71 @@
+import PySimpleGUI as sg
+from tools import center, resource_path, todays_date
+
+title = 'Camp Buddy Toolbox'
+description = 'Camp Buddy Toolbox allows you to extract assets and scripts of Camp Buddy easily!'
+short_description = 'Allows you to extract assets and scripts of Camp Buddy easily!'
+version_num = '0.3 Alpha'
+website_link = ''
+github_username = 'lonewanderer27'
+nickname = 'Jay'
+license_notice = f'''{title} is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+{title} is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with {title}.  If not, see http://www.gnu.org/licenses'''
+
+sg.theme('DarkBrown3')
+
+def get_about_window():
+    about_tab = [
+        center([sg.Image(resource_path('icon.png'))]),
+        center([sg.Text(text=title)]),
+        center([sg.Text(text=version_num)]),
+        center([sg.Text(text=short_description)]),
+        center([sg.Text(text='Website')]),
+        center([sg.Text(text=f'Copyright © {todays_date.year} {github_username} ({nickname})')]),
+        center([sg.Text(text='This program comes with absolutely no warranty.\nSee the GNU General Public License 3 for details.'
+        )])
+    ]
+
+    left_credits_column = [
+        [sg.Text('Created by:')],
+        [sg.Text('Artwork by:\n')],
+        [sg.Text('Used Libraries\n\n\n')]
+    ]
+
+    right_credits_column = [
+        [sg.Text(f'{github_username} ({nickname})')],
+        [sg.Text(f'Font Awesome\n{github_username} ({nickname})')],
+        [sg.Text('unrpa\nrpatool\nPySimpleGUI')]
+    ]
+
+    credits_tab = [
+        center([sg.Image(resource_path('icon.png'))]),
+        center([sg.Text(title)]),
+        center([sg.vtop(sg.Column(left_credits_column)), sg.vtop(sg.Column(right_credits_column))])
+    ]
+
+    license_tab = [
+        center([sg.Image(resource_path('icon.png'))]),
+        center([sg.Text(title)]),
+        center([sg.Multiline(default_text=license_notice,justification='center', disabled=True, size=(50,8))])
+    ]
+
+    about_window_layout = [
+        [sg.TabGroup([
+            [sg.Tab(title='About', layout=about_tab), sg.Tab(title='Credits', layout=credits_tab), sg.Tab(title='License', layout=license_tab)]
+        ], tab_location='top')]
+    ]
+
+    return sg.Window(title=f'About {title}', layout=about_window_layout)
+
+def about():
+    while True:
+        about_window = get_about_window()
+        event3, values3 = about_window.read()
+
+        if event3 == sg.WIN_CLOSED:
+            break
+        
+    about_window.close()
---
+++ src/tools.py
@@ -0,0 +1,60 @@
+import platform
+import subprocess
+import os
+import sys
+import ntpath
+from rpatool import RenPyArchive
+import PySimpleGUI as sg
+from pprint import pprint
+from datetime import date
+
+def center(elements):
+    return [sg.Push(), *elements, sg.Push()]
+
+def open_folder(path: str):
+    if platform.system() == "Windows":
+        os.startfile(path)
+    elif platform.system() == "Darwin":
+        subprocess.Popen(["open", path])
+    else:
+        subprocess.Popen(["xdg-open", path])
+
+def resource_path(filename):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, filename)
+    else:
+        return filename
+
+todays_date = date.today()
+
+def print_debug_info(window, event, values):
+    print(f'\n\n{window} Event:\n')
+    pprint(event, indent=2)
+    print(f'\n\n{window} Values:\n')
+    pprint(values, indent=2)
+    print('')
+
+def get_filename_from_path(rpapath: str) -> str:
+    return ntpath.basename(rpapath)
+
+def valid_path(rpapath: str):
+    if not len(rpapath) == 0:
+        return True
+    else:
+        return False
+
+def valid_rpa_file(rpafile: str) -> tuple:
+    rpaprocessor = RenPyArchive()
+    try:
+        rpaprocessor.load(filename=rpafile)
+    except OSError as e:
+        return False, "Error. File does not exist or permission denied"
+    except ValueError as e:
+        return False, "Error. Invalid archive"
+    else:
+        return True, f"loading {get_filename_from_path(rpafile)}"
+
+def list_rpa_files(rpafile: str) -> list:
+    rpaprocessor = RenPyArchive(rpafile)
+    files_list = sorted(rpaprocessor.list())
+    return files_list
